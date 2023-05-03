a25d1faad46a6d7b8100cd808fee01ff359fd3e6
Igor Mogilnitskiy <igmo71@outlook.com>
2023-05-03 22:03:42 +0300

    DocOutDto

---
+++ Pie/Data/Models/DocOutDto.cs
@@ -0,0 +1,28 @@
+﻿namespace Pie.Data.Models
+{
+    public class DocOutDto
+    {
+        public Guid id { get; set; }
+        public string? name { get; set; }
+        public string? number { get; set; }
+        public DateTime dateTime { get; set; }
+        public bool active { get; set; }
+        public Guid warehouseId { get; set; }
+        public int statusKey { get; set; }
+        public int queueKey { get; set; }
+        public List<ProductDto>? products { get; set; }
+        public List<BaseDocDto>? baseDocs { get; set; }
+    }
+
+    public class ProductDto
+    {
+        public Guid productId { get; set; }
+        public int count { get; set; }
+    }
+
+    public class BaseDocDto
+    {
+        public Guid baseDocId { get; set; }
+        public string? name { get; set; }
+    }
+}
