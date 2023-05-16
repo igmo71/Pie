7a86956c44ad8fb5f2d9be294bae68ca11aa093e
Igor Mogilnitskiy <igmo71@outlook.com>
2023-05-16 16:55:00 +0300

    DocOutProductHistoryService.

---
+++ Pie/Data/Services/Out/DocOutHistoryService.cs
@@ -0,0 +1,30 @@
+﻿using Pie.Data.Models.Out;
+using Pie.Data.Services.Application;
+
+namespace Pie.Data.Services.Out
+{
+    public class DocOutHistoryService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ApplicationUserService _userService;
+
+        public DocOutHistoryService(ApplicationDbContext context, ApplicationUserService userService)
+        {
+            _context = context;
+            _userService = userService;
+        }
+        public async Task CreateAsync(DocOut doc)
+        {
+            DocOutHistory docHistory = new DocOutHistory()
+            {
+                DateTime = DateTime.Now,
+                UserId = _userService.CurrentUserId,
+                DocId = doc.Id,
+                StatusKey = doc.StatusKey
+            };
+
+            _context.Add(docHistory);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
