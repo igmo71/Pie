0d53079cfb028b45987fa207fd38ccd4d3db0629
Igor Mogilnitskiy <igmo71@outlook.com>
2023-05-16 16:13:28 +0300

    Add LineNumber.

---
+++ Pie/Data/Models/Out/DocOutVm.cs
@@ -0,0 +1,10 @@
+﻿namespace Pie.Data.Models.Out
+{
+    public class DocOutVm
+    {
+        public DocOut? Item { get; set; }
+        public string? Barcode { get; set; }
+        public string? UserName { get; set; }
+        public float Weight => Item != null ? Item.Products.Sum(x => x.Weight) : default;
+    }
+}
